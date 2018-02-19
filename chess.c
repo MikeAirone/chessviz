b5e31ad190c2fd0949e2a1522898dec3e008cd9d
Онищенко Анастасия Валерьевна <ip712s16@mwp11.csc.local>
2018-02-19 08:59:42 +0700

    output chessviz

--- chess.c
+++ chess.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main()
 {
-printf (" a b c d e f g h/n");
-printf (" r b n q k n b r/n p p p p p p p p/n");
-printf ("1/n2/n3/n4/n5/n6/n");
-printf ("7 p p p p p p p p /n8 r b n q k n b r/n");
+printf (" a b c d e f g h\n");
+printf ("1");
+printf (" r b n q k n b r\n");
+printf ("2");
+printf (" p p p p p p p p\n");
+printf ("3\n4\n5\n6\n");
+printf ("7 p p p p p p p p \n8 r b n q k n b r\n");
 return 0;
 }
