fc07a141412ab194f7f1aa17a72e8da4d1670336
MikeAirone <onishencko2011@yandex.ru>
2018-05-28 09:03:19 +0700

    Add tests

--- test/main.c
+++ test/main.c
@@ -5,6 +5,8 @@
 #include <input.h>
 #include <ctest.h>
 
+char deck[8][8];
+
 CTEST(Syntax, Correct) 
 {
 	int result = input_check("a2a8");
@@ -18,7 +20,71 @@ CTEST(Syntax, Incorrect)
 	int expected = 1; 
 	ASSERT_EQUAL(expected, result);
 }
+CTEST(Pawn, Correct) 
+{
+    	board_print(deck);
+    	char move[4]="a2a4";
+	int moveint [4];   
+	input(move, moveint);
+    	int result = pawn(deck, moveint);
+    	int expected = 0;
+    	ASSERT_EQUAL(expected, result);
+}
 
+CTEST(Pawn, Incorrect) 
+{
+    	board_print(deck);
+    	char move[4]="a2a5";
+	int moveint [4];    
+	input(move, moveint);
+    	int result = pawn(deck, moveint);
+    	int expected = 1;
+   	ASSERT_EQUAL(expected, result);
+}
+
+
+CTEST (Check_Figure, Correct)
+{
+	board_print(deck);
+    	char move[4]="a2a5";
+	int moveint [4];    
+	input(move, moveint);
+	int result = check_figure(deck, moveint);
+    	int expected = 0;
+    	ASSERT_EQUAL(expected, result);
+
+}
+CTEST (Check_Figure, Incorrect)
+{
+  	board_print(deck);
+    	char move[4]="a5a6";
+	int moveint [4];   
+	input(move, moveint);
+	int result = check_figure(deck, moveint);
+    	int expected = 1;
+    	ASSERT_EQUAL(expected, result);
+}
+CTEST(Pawn1, Correct) 
+{
+    	board_print(deck);
+    	char move[4]="a7a5";
+	int moveint [4];    
+	input(move, moveint);
+    	int result = PAWN(deck, moveint);
+    	int expected = 0;
+    	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Pawn1, Incorrect) 
+{
+   	board_print(deck);
+   	char move[4]="a7a3";
+	int moveint [4];    
+	input(move, moveint);
+    	int result = PAWN(deck, moveint);
+    	int expected = 1;
+    	ASSERT_EQUAL(expected, result);
+}
 int main(int argc, const char** argv) 
 {
     	return ctest_main(argc, argv);
